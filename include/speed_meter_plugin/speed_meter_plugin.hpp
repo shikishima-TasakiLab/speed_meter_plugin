f6e550db09dae8e8ed80c48a0756d1b011bfd21e
shikishima-TasakiLab <160442065@ccalumni.meijo-u.ac.jp>
2024-10-02 14:42:11 +0000

    最低限のRVizへの描画

--- include/speed_meter_plugin/speed_meter_plugin.hpp
+++ include/speed_meter_plugin/speed_meter_plugin.hpp
@@ -3,17 +3,30 @@
 
 #include "speed_meter_plugin/visibility_control.h"
 
+#ifndef Q_MOC_RUN
+#include <rclcpp/rclcpp.hpp>
+#include <rviz_common/panel.hpp>
+#include <rviz_common/display_context.hpp>
+#endif
+
 namespace speed_meter_plugin
 {
 
-class SpeedMeterPlugin
-{
-public:
-  SpeedMeterPlugin();
+  class SpeedMeterPlugin : public rviz_common::Panel
+  {
+    Q_OBJECT
+  public:
+    SpeedMeterPlugin(QWidget *parent = nullptr);
+    ~SpeedMeterPlugin();
 
-  virtual ~SpeedMeterPlugin();
-};
+    void onInitialize() override;
+    void load(const rviz_common::Config &config) override;
+    void save(rviz_common::Config config) const override;
 
-}  // namespace speed_meter_plugin
+  protected:
+    rclcpp::Node::SharedPtr nh_;
+  };
 
-#endif  // SPEED_METER_PLUGIN__SPEED_METER_PLUGIN_HPP_
+} // namespace speed_meter_plugin
+
+#endif // SPEED_METER_PLUGIN__SPEED_METER_PLUGIN_HPP_
--- src/speed_meter_plugin.cpp
+++ src/speed_meter_plugin.cpp
@@ -3,12 +3,29 @@
 namespace speed_meter_plugin
 {
 
-SpeedMeterPlugin::SpeedMeterPlugin()
-{
-}
+  SpeedMeterPlugin::SpeedMeterPlugin(QWidget *parent)
+      : rviz_common::Panel(parent)
+  {
+  }
 
-SpeedMeterPlugin::~SpeedMeterPlugin()
-{
-}
+  SpeedMeterPlugin::~SpeedMeterPlugin()
+  {
+  }
 
-}  // namespace speed_meter_plugin
+  void SpeedMeterPlugin::onInitialize()
+  {
+    nh_ = getDisplayContext()->getRosNodeAbstraction().lock()->get_raw_node();
+  }
+
+  void SpeedMeterPlugin::load(const rviz_common::Config &config)
+  {
+  }
+
+  void SpeedMeterPlugin::save(rviz_common::Config config) const
+  {
+  }
+
+} // namespace speed_meter_plugin
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(speed_meter_plugin::SpeedMeterPlugin, rviz_common::Panel)
